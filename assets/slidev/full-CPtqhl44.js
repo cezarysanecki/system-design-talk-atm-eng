4aa9fe3eee2832b9c395f1156c9cdbb8c075bebb
Cezary Sanecki <saneckicezary@gmail.com>
2026-09-24 17:36:34 +0200

    Remove Super Senior Java promo

--- assets/slidev/full-CPtqhl44.js
+++
@@ -1,1 +0,0 @@
-import{B as e,L as t,y as n}from"../modules/shiki-CyXfYLBM.js";import{gt as r}from"../index-DN9zVh9d.js";var i={},a={class:`slidev-layout full w-full h-full`};function o(r,i){return t(),n(`div`,a,[e(r.$slots,`default`,{class:`w-full h-full`})])}var s=r(i,[[`render`,o]]);export{s as t};
